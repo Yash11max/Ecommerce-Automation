095930cca74bce9348874a9180c72fb3bb00a4fa
Yash11max <shankhwalkary11@gmail.com>
2026-02-03 17:02:09 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -3,10 +3,30 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
+  ############################################
+  # ✅ EKS ACCESS ENTRY (REPLACES aws-auth)
+  ############################################
+  access_entries = {
+    tws_admin = {
+      principal_arn = "arn:aws:iam::715841342014:user/tws-ecom"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
+    }
+  }
+
+  ############################################
+  # EKS ADDONS
+  ############################################
   cluster_addons = {
     coredns = {
       most_recent = true
@@ -14,29 +34,30 @@ module "eks" {
     kube-proxy = {
       most_recent = true
     }
-    vpc-cni = {
+ vpc-cni = {
       most_recent = true
     }
   }
 
+  ############################################
+  # NETWORKING
+  ############################################
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.public_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-  # EKS Managed Node Group(s)
-
+  ############################################
+  # NODE GROUP DEFAULTS
+  ############################################
   eks_managed_node_group_defaults = {
-
     instance_types = ["t2.large"]
-
     attach_cluster_primary_security_group = true
-
   }
 
-
-
+  ############################################
+  # MANAGED NODE GROUP
+  ############################################
   eks_managed_node_groups = {
-
     tws-demo-ng = {
       min_size     = 2
       max_size     = 3
@@ -44,23 +65,23 @@ module "eks" {
 
       instance_types = ["t2.large"]
       capacity_type  = "SPOT"
-
-      disk_size = 35 
-      use_custom_launch_template = false  # Important to apply disk size!
+   disk_size = 35
+      use_custom_launch_template = false
 
       tags = {
-        Name = "tws-demo-ng"
+        Name        = "tws-demo-ng"
         Environment = "dev"
-        ExtraTag = "e-commerce-app"
+        ExtraTag    = "e-commerce-app"
       }
     }
   }
- 
+
   tags = local.tags
-
-
 }
 
+############################################
+# OPTIONAL: FETCH EKS NODE EC2 INSTANCES
+############################################
 data "aws_instances" "eks_nodes" {
   instance_tags = {
     "eks:cluster-name" = module.eks.cluster_name
@@ -73,3 +94,5 @@ data "aws_instances" "eks_nodes" {
 
   depends_on = [module.eks]
 }
+
+
